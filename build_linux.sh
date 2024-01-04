cf0a714f255b5b23ef6c8bc93ae865b8209eeae0
L4p1s <124094769+PommePain@users.noreply.github.com>
2024-01-04 22:41:34 +0100

    Update build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -1,8 +1,9 @@
-mkdir -p linux-build
-cd linux-build
-cmake -G "Unix Makefiles" ..
+mkdir -p linux-build && cd linux-build && cmake -G "Unix Makefiles" ..
+make clean
 make
 cp app ../app
 mkdir -p ../linux-output
 cp app ../linux-output/app
 cp -R style/ ../linux-output/
+cp -R components ../linux-output/
+echo "Build finished!"
