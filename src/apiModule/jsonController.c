da9a97869238b20b5613a1c592e8f5b19885b9e1
PommePain <124094769+PommePain@users.noreply.github.com>
2024-01-17 00:52:12 +0100

    Update json controller add parseJsonToLeaksList

--- src/apiModule/jsonController.c
+++ src/apiModule/jsonController.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <cjson/cJSON.h>
 #include "../../includes/jsonController.h"
 
@@ -16,6 +17,32 @@ void printJson(cJSON *item) {
     }
 }
 
-void parseJsonFromString(char *str) {
-    
+LeaksList *parseJsonToLeaksList(cJSON *json, char *login) {
+    LeaksList *list = (LeaksList *) malloc(sizeof(LeaksList));
+    list->count = 0;
+    list->credentialLeaks = NULL;
+
+    const cJSON *successStatusItem = cJSON_GetObjectItem(json, "success");
+    if (cJSON_IsBool(successStatusItem) && successStatusItem->valueint == 1) {
+        int index = 0;
+        cJSON *sources = cJSON_GetObjectItem(json, "sources");
+        cJSON *foundCount = cJSON_GetObjectItem(json, "found");
+        cJSON *source = NULL;
+
+        list->credentialLeaks = (CredentialLeak *) malloc(sizeof(CredentialLeak) * foundCount->valueint);
+        cJSON_ArrayForEach(source, sources) {
+            cJSON *name = cJSON_GetObjectItem(source, "name");
+            cJSON *date = cJSON_GetObjectItem(source, "date");
+            if (name != NULL) {
+                printf("name ==== %s\n", cJSON_Print(name));
+                list->credentialLeaks[index].website = strdup(cJSON_Print(name));
+                list->credentialLeaks[index].login = strdup(login);
+                list->credentialLeaks[index].leakDate = strdup(cJSON_Print(date));
+                list->count += 1;
+                index++;
+            }
+        }
+    }
+
+    return list;
 }
