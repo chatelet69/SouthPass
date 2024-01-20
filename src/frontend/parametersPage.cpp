4d22a94fae26a888bca0e719faf83ec543da5671
SpyKo_ <mathfremiot@gmail.com>
2024-01-20 14:02:17 +0100

    add edit pwd master

--- src/frontend/parametersPage.cpp
+++ src/frontend/parametersPage.cpp
@@ -41,20 +41,23 @@ ParametersPage::ParametersPage(ApplicationController *appController, QApplicatio
 
     QLabel * secuTitle = new QLabel("Sécurité :");
     QWidget * secuBox = new QWidget();
-    QHBoxLayout * secuLayout = new QHBoxLayout(secuBox);
+    QVBoxLayout * secuLayout = new QVBoxLayout(secuBox);
+    QWidget * pwdBox = new QWidget();
+    QHBoxLayout * pwdLayout = new QHBoxLayout(pwdBox);
+
     QLabel * editPwdTitle = new QLabel("Modifier votre mot de passe :");
     QPushButton * editPwdBtn = new QPushButton("Modifier");
-    /*QFormLayout *editMasterPwd = new QFormLayout();
-    QLineEdit *masterPwdInput = new QLineEdit;
-    masterPwdInput->setEchoMode(QLineEdit::Password);
-    QLineEdit *verifMasterPwdInput = new QLineEdit;
-    verifMasterPwdInput->setEchoMode(QLineEdit::Password);
-    QPushButton *editMasterPwdBtn = new QPushButton("Modifier");
-    editMasterPwd->addRow(masterPwdInput);
-    editMasterPwd->addRow(verifMasterPwdInput);
-    editMasterPwd->addRow(editMasterPwdBtn);*/
-    secuLayout->addWidget(editPwdTitle);
-    secuLayout->addWidget(editPwdBtn);
+    pwdLayout->addWidget(editPwdTitle);
+    pwdLayout->addWidget(editPwdBtn);
+    secuLayout->addWidget(pwdBox);
+    QWidget * pwdBox2 = new QWidget();
+    QHBoxLayout * pwdLayout2 = new QHBoxLayout(pwdBox2);
+    QLabel * editMasterPwdTitle = new QLabel("Modifier votre mot de passe maitre :");
+    QPushButton * editMasterPwdBtn = new QPushButton("Modifier");
+    pwdLayout2->addWidget(editMasterPwdTitle);
+    pwdLayout2->addWidget(editMasterPwdBtn);
+    secuLayout->addWidget(pwdBox2);
+
 
     QLabel * lookTitle = new QLabel("Apparence :");
     QWidget * darkOrLightBox = new QWidget();
@@ -62,11 +65,9 @@ ParametersPage::ParametersPage(ApplicationController *appController, QApplicatio
     QRadioButton * darkMode = new QRadioButton("Dark mode");
     QRadioButton * lightMode = new QRadioButton("Light Mode");
     if(getThemePreference() == 1){
-        printf("ok");
         darkMode->setChecked(true);
     }else{
         lightMode->setChecked(true);
-        printf("ko");
     }
 
     darkOrLightLayout->addWidget(darkMode);
@@ -100,28 +101,7 @@ ParametersPage::ParametersPage(ApplicationController *appController, QApplicatio
     connect(extUrlBtn, &QPushButton::clicked, [=](){QDesktopServices::openUrl(QUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));});
     connect(darkMode, &QRadioButton::clicked, [=](){changeThemeMode(appController, app);});
     connect(lightMode, &QRadioButton::clicked, [=](){changeThemeMode(appController, app);});
-    connect(editPwdBtn, &QPushButton::clicked, [=](){
-        // CONVERSION DES INPUT EN STRING POUR LE C
-        /*char Pwd[50];
-        strcpy(Pwd, ParametersPage::convertStringForC(masterPwdInput));
-        char verifPwdString[50];
-        strcpy(verifPwdString, ParametersPage::convertStringForC(verifMasterPwdInput));
 
-        int res;
-        res = verifPwd(dbCon, Pwd, verifPwdString, tokenInfos->email);
-        if(res == 3){
-            QMessageBox::warning(this,"Erreur" ,"Une erreur est apparue lors de la modification du mot de passe");
-        }else if(res == 2){
-            QMessageBox::warning(this,"Erreur" ,"Mots de passes différents, veuillez re essayer.");
-        }else if(res == 1){
-            QMessageBox::warning(this,"Erreur" ,"Vos mots de passes doivent contenir au moins 1 lettre, 1 chiffre et 1 caractère spécial");
-        }else if(res == 0){
-            QMessageBox::information(this,"Mot de passe maitre modifié avec succès !" ,"Veuillez vous reconnecter.");
-            app->disconnect();
-            return 0;
-        }
-        return 1;*/
-    });
 }
 
 char * ParametersPage::convertStringForC(QLineEdit * string){
