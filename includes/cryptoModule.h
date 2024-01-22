ff0d721eee437dda5c21755a9e306203227d6052
PommePain <124094769+PommePain@users.noreply.github.com>
2024-01-22 17:36:28 +0100

    delete cryptoModule

--- includes/cryptoModule.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRYPTOGRAPHY_MODULE_H
-#define CRYPTOGRAPHY_MODULE_H
-
-void handleErrors(void);
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,unsigned char *iv, unsigned char *plaintext);
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,unsigned char *iv, unsigned char *ciphertext);
-unsigned char *encryptString(const char *plain, char *test);
-
-#endif
