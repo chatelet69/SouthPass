d732151863461fa58146cfb78b0ff5229e3e0b45
PommePain <124094769+PommePain@users.noreply.github.com>
2024-02-01 12:46:35 +0100

    removed useless scripts

--- build2.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start /B cmake -B ./test-build -G "MinGW Makefiles" && mingw32-make.exe -C ./test-build/ && copy .\test-build\app.exe .
--- build_linux.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p linux-build && cd linux-build && cmake -G "Unix Makefiles" ..
-make clean
-make
-cp app ../app
-mkdir -p ../linux-output
-cp app ../linux-output/app
-cp -R style/ ../linux-output/
-cp -R components ../linux-output/
-echo "Build finished!"
